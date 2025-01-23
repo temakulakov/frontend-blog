ee506407200ac02a8eed231c34905b60fcd2fa84
Tema Kulakov <temakulakov@MacBook-Air-Tema.local>
2025-01-23 12:59:38 +0300

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,7 @@
 'use client';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 
 const Example: React.FC = () => {
-  const [data, setData] = useState<any | null>(null);
-
-  useEffect(() => {
-    // Данный код сработает только на клиенте
-    axios
-      .get('https://gachimuchi.fun/api/posts')
-      .then((res) => setData(res.data));
-  }, []);
-
-  return <div>Data: {data || 'Loading...'}</div>;
+  return <div></div>;
 };
 
 export default Example;
